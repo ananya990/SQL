4329a161e941eecf532c79d6ac69a6fb877b3922
Ananya V A <142164935+ananya990@users.noreply.github.com>
2024-06-08 23:13:09 +0530

    Create Leetcode_175_Combine_Two_Tables.sql

---
+++ Leetcode_175_Combine_Two_Tables.sql
@@ -0,0 +1,17 @@
+/* Table : Person                  Table : Address
++-------------+---------+         +-------------+---------+
+| Column Name | Type    |         | Column Name | Type    |
++-------------+---------+         +-------------+---------+
+| personId    | int     |         | addressId   | int     |
+| lastName    | varchar |         | personId    | int     |
+| firstName   | varchar |         | city        | varchar |
++-------------+---------+         | state       | varchar |
+                                  +-------------+---------+
+
+Write a solution to report the first name, last name, city, and 
+state of each person in the Person table. If the address of a 
+personId is not present in the Address table, report null instead. */
+
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a ON p.personId = a.personId;
